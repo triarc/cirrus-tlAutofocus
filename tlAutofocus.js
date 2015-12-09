5f33201351d503d886d7bd8adc2087d706da49af
Sven Arnold <sa@triarc-labs.com>
2015-12-09 10:48:48 +0100

    Bumped version to 1.0.3

--- tlAutofocus.js
+++ tlAutofocus.js
@@ -2,17 +2,18 @@ var Triarc;
 (function (Triarc) {
     var Web;
     (function (Web) {
-        angular.module("tlAutofocus", []).directive("tlAutofocus", ["$timeout", function ($timeout) {
-            return {
-                restrict: "A",
-                link: function ($scope, $element) {
-                    $timeout(function () {
-                        $element[0].focus();
-                        $element[0].select();
-                    });
-                }
-            };
-        }]);
+        angular.module("tlAutofocus", [])
+            .directive("tlAutofocus", ["$timeout", function ($timeout) {
+                return {
+                    restrict: "A",
+                    link: function ($scope, $element) {
+                        $timeout(function () {
+                            $element[0].focus();
+                            $element[0].select();
+                        });
+                    }
+                };
+            }]);
     })(Web = Triarc.Web || (Triarc.Web = {}));
 })(Triarc || (Triarc = {}));
 
